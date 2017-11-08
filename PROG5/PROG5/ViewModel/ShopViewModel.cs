531cae66471b50f7b8fd1d5eedcaacbe675f2ec8
Patrick Spek <p.spek@tyil.nl>
2017-11-07 22:20:27 -0800

    Update shop buttons to be disabled by default

--- PROG5/PROG5/ViewModel/ShopViewModel.cs
+++ PROG5/PROG5/ViewModel/ShopViewModel.cs
@@ -11,12 +11,31 @@ namespace PROG5.ViewModel
     public class ShopViewModel : ViewModelBase
     {
         public ICommand CloseCommand { get; set; }
+
         public ICommand ItemCommand { get; set; }
+
         public ICommand TypeCommand { get; set; }
+
         public ICommand BuyCommand { get; set; }
+
+        public ICommand SellCommand { get; set; }
+
         public NinjaViewModel Ninja { get; set; }
-        public EquipmentViewModel SelectedEquipment { get; set; }
+
+        public EquipmentViewModel SelectedEquipment {
+            get => _selectedEquipment;
+
+            set
+            {
+                _selectedEquipment = value;
+
+                // TODO: Check if the currently selected ninja already has the currently selected equipment
+                hasEquipment = false;
+            }
+        }
+
         public EquipmentTypeViewModel SavedEquipmentTypeViewModel { get; set; }
+
         public EquipmentTypeViewModel SelectedEquipmentType
         {
             get => SavedEquipmentTypeViewModel;
@@ -29,27 +48,45 @@ namespace PROG5.ViewModel
                 SavedEquipmentTypeViewModel = value;
             }
         }
+
         public ObservableCollection<EquipmentTypeViewModel> EquipmentType => TypeRepository.GetAll();
+
         public ObservableCollection<EquipmentViewModel> Equipment { get; set; }
+
         public IEquipmentTypeRepository TypeRepository { get; set; }
+
         public IEquipmentRepository EquipmentRepository { get; set; }
+
         public INinjaEquipmentRepository NinjaEquipmentRepository { get; set; }
-        public ShopViewModel(ShowNinjaViewModel sh,
+
+        private bool hasEquipment;
+
+        private EquipmentViewModel _selectedEquipment;
+
+        public ShopViewModel(
+            ShowNinjaViewModel sh,
             IEquipmentTypeRepository types,
             IEquipmentRepository equipment,
-            INinjaEquipmentRepository link)
-        {
-            Ninja = sh.SelectedNinja;
+            INinjaEquipmentRepository link
+        ) {
+            hasEquipment = false;
+            Ninja = sh.SelectedNinja; // TODO: Currently only set on the FIRST time the shop opens, never updated, it should update every time the shop opens
             TypeRepository = types;
             NinjaEquipmentRepository = link;
             EquipmentRepository = equipment;
             CloseCommand = new RelayCommand(Close);
             ItemCommand = new RelayCommand(ItemManagementWindow);
             TypeCommand = new RelayCommand(TypeManagementWindow);
-            BuyCommand = new RelayCommand(BuyItem);
+            BuyCommand = new RelayCommand(BuyItem, () => SelectedEquipment != null && SelectedEquipment.Gold <= Ninja.Gold && !hasEquipment);
+            SellCommand = new RelayCommand(SellItem, () => SelectedEquipment != null && hasEquipment);
         }
 
         public void BuyItem()
+        {
+
+        }
+
+        public void SellItem()
         {
 
         }
