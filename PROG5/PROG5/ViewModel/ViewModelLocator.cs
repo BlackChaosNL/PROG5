5df7e55cf04ede221102f03ea1feda8799bd769e
Jeroen Vijgen <jjvijgen@gmail.com>
2017-11-17 01:04:10 +0100

    Item management

--- PROG5/PROG5/ViewModel/ViewModelLocator.cs
+++ PROG5/PROG5/ViewModel/ViewModelLocator.cs
@@ -1,17 +1,3 @@
-/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:PROG5"
-                           x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
-*/
-
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 using PROG5.Repository.Interfaces;
@@ -22,6 +8,11 @@ namespace PROG5.ViewModel
 {
     public class ViewModelLocator
     {
+        public ShowNinjaViewModel Main => ServiceLocator.Current.GetInstance<ShowNinjaViewModel>();
+        public ShopViewModel Shop => ServiceLocator.Current.GetInstance<ShopViewModel>();
+        public AddNinjaDialogViewModel Popup => ServiceLocator.Current.GetInstance<AddNinjaDialogViewModel>();
+        public TypeManagementViewModel Type => ServiceLocator.Current.GetInstance<TypeManagementViewModel>();
+        public ItemManagementViewModel Item => ServiceLocator.Current.GetInstance<ItemManagementViewModel>();
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
@@ -44,19 +35,28 @@ namespace PROG5.ViewModel
             SimpleIoc.Default.Register<EquipmentViewModel>();
             SimpleIoc.Default.Register<AddNinjaDialogViewModel>();
             SimpleIoc.Default.Register<TypeManagementViewModel>();
+            SimpleIoc.Default.Register<ItemManagementViewModel>();
         }
-
-        public ShowNinjaViewModel Main => ServiceLocator.Current.GetInstance<ShowNinjaViewModel>();
-
-        public ShopViewModel Shop => ServiceLocator.Current.GetInstance<ShopViewModel>();
-
-        public AddNinjaDialogViewModel Popup => ServiceLocator.Current.GetInstance<AddNinjaDialogViewModel>();
-
-        public TypeManagementViewModel Type => ServiceLocator.Current.GetInstance<TypeManagementViewModel>();
-
         public static void Cleanup()
         {
+            SimpleIoc.Default.Unregister<DatabaseModelContainer>();
+
+            // Set some repositories for data access.
             SimpleIoc.Default.Unregister<INinjaRepository>();
+            SimpleIoc.Default.Unregister<IEquipmentRepository>();
+            SimpleIoc.Default.Unregister<IEquipmentTypeRepository>();
+            SimpleIoc.Default.Unregister<INinjaEquipmentRepository>();
+
+            // Add the VMs, we love some virtual machines.
+            SimpleIoc.Default.Unregister<NinjaViewModel>();
+            SimpleIoc.Default.Unregister<ShowNinjaViewModel>();
+            SimpleIoc.Default.Unregister<ShopViewModel>();
+            SimpleIoc.Default.Unregister<NinjaEquipmentViewModel>();
+            SimpleIoc.Default.Unregister<EquipmentTypeViewModel>();
+            SimpleIoc.Default.Unregister<EquipmentViewModel>();
+            SimpleIoc.Default.Unregister<AddNinjaDialogViewModel>();
+            SimpleIoc.Default.Unregister<TypeManagementViewModel>();
+            SimpleIoc.Default.Unregister<ItemManagementViewModel>();
         }
     }
 }
