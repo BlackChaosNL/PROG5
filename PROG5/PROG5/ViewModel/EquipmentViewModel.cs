f99f38d129f0ed91852345f068c2963979d66bcd
Patrick Spek <p.spek@tyil.nl>
2017-11-23 06:51:27 -0800

    Add newlines to improve readability

--- PROG5/PROG5/ViewModel/EquipmentViewModel.cs
+++ PROG5/PROG5/ViewModel/EquipmentViewModel.cs
@@ -3,11 +3,17 @@
     public class EquipmentViewModel
     {
         public int Id { get; set; }
+
         public int Int { get; set; }
+
         public int Str { get; set; }
+
         public int Agi { get; set; }
+
         public int Gold { get; set; }
+
         public string Name { get; set; }
+
         public EquipmentTypeViewModel EquipmentTypeViewModel { get; set; }
     }
 }
